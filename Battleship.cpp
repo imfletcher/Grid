869775e1301b616f68b58deb89ebe271a54ac570
imfletcher <golfinthesnow@hotmail.com>
2018-07-05 22:38:40 -0500

    Add files via upload

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : Battleship.cpp
-// Author      : 
-// Version     :
+// Author      : Greg Mattson
+// Version     : 0.001
 // Copyright   : Your copyright notice
 // Description : Hello World in C, Ansi-style
 //============================================================================
@@ -10,32 +10,39 @@
 #include <string>
  using namespace std;
 
-class Board 
+class Board
 {
-   private: 
-      int board[10][10];
+   private:
+      const char* board[10][10];
       int x;
       int y;
+
+      void addShip(string shipName, const char* shipType, int length){
+         cout << "Enter the X co-ordinate for " + shipName;
+         cin >> x;
+         cout << "Enter the Y co-ordinate" + shipName;
+         cin >> y;
+         board[x-1][y-1] = "C";
+      }
 
    public:
       void createBoard() {
          for (int i=0;i<10;i++) {
 		    for (int j=0; j<10; j++) {
-			   board[i][j] = 0;
+			   board[i][j] = "-";
 		    }
 		 }
 	  }
-      
+
       void setupBoard() {
+	      string shipToAddName = "Carrier";
+	      const char* shipToAddType = "C";
+	      int shipToAddLength = 1;
 	      for (int i=0;i<5;i++) {
-             cout << "Enter the X co-ordinate";
-             cin >> x;
-             cout << "Enter the Y co-ordinate";
-             cin >> y;
-             board[x-1][y-1] = 1;
+            addShip(shipToAddName, shipToAddType, shipToAddLength);
           }
 	  }
-	
+
       void displayBoard() {
 	     for (int i=0;i<10;i++) {
 		    for (int j=0; j<10; j++) {
@@ -46,11 +53,11 @@ class Board
       }
 };
 
-int main() 
+int main()
 {
    Board PlayerABoard;
    Board PlayerBBoard;
-   
+
    PlayerABoard.createBoard();
    PlayerABoard.displayBoard();
    PlayerABoard.setupBoard();
